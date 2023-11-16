b9f5269399989e22ffb1a3b746db7d324ef9ab8c
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-16 17:17:59 +0000

    todo list with trash button funcionality and todolist counter

--- src/js/component/Input.jsx
+++ src/js/component/Input.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-const Input = ({todo}) => {
+const Input = ({ todo }) => {
 
   return (
-    <div>
-     
+    <div className= "container-fluid">
 
-      <div className="todo-items d-flex justify-content-around align-items-center bg-light shadow-sm bg-body-tertiary rounded m-auto" style={{ width: "30rem" }}>
-      <i className="fa-solid fa-check fa-sm" style={{color: "#8e9ab0"}}></i>
+
+      <div className="todo-items justify-content-around align-items-center bg-light shadow-sm bg-body-tertiary rounded m-auto" style={{ width: "30rem" }}>
+        <i className="fa-solid fa-check fa-sm" style={{ color: "#8e9ab0" }}></i>
         <span className="todos-text">{todo}</span>
         <button className="todos-delete btn">
-          <i className="fa-solid fa-square-minus" style={{ color: "#de1818" }}></i>
+          <i className="trash fa-solid fa-square-minus" style={{ color: "#de1818" }}></i>
         </button>
-      
+  
       </div>
+      <div className="total-todo text-center justify-content-around fs-6 fw-light"></div>
       
-      </div>
+    </div>
 
   );
 }
-
 
 export default Input;
--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -2,44 +2,49 @@ import React, { useState } from "react";
 import Input from "../component/Input.jsx";
 
 const ToDoList = () => {
-
+  const [inputValue, setInputValue] = useState("")
   const [todos, setTodos] = useState([]);
-  const [inputValue, setInputValue] = useState ([])
 
   const handleKeyPress = (e) => {
-    
+
     if (e.key === "Enter") {
       e.preventDefault()
-      setTodos([...todos, e.target.value]) 
+      setTodos([...todos, e.target.value])
+      setInputValue("")
     }
   };
 
-  return (
-    <div className="List-Your-Todo container-fluid text-center mt-5 mb-5">
-      <form className="list-input">
-        <h1 className="list-title">To Do List</h1>
+  const onClick = (index) => {
+    setTodos(todos.filter((todo, currentIndex) => index !== currentIndex));
 
-        <input
-        className="input-control container-fluid border-dark-subtle"
-        style={{ width: "30rem" }}
-        type="text" 
-        placeholder="What do you want to do?"
-        aria-label="todo list input field"
-        onChange={(e) => setInputValue(e.target.value)}
-        value = {inputValue}
-        onKeyDown={handleKeyPress}
-      />
+    return (
+      <div className="List-Your-Todo container-fluid text-center mt-5 mb-5">
+        <form className="list-input">
+          <h1 className="list-title">To Do List</h1>
 
-        {todos.map((todo) => (
-        <Input 
-          todo={todo}
-  
-         />
-        
-              ))}
-            </form>
-          </div>
-        );
-      };
+          <input
+            className="input-control container-fluid border-dark-subtle"
+            style={{ width: "30rem" }}
+            type="text"
+            placeholder="What do you want to do?"
+            aria-label="todo list input field"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={handleKeyPress}
 
+          />
+
+          {todos.map((todo) => (
+            <Input
+              todo={todo}
+              trash={(todo) => onClick(index)}
+              total-Todo={todos.length}
+            />
+          ))};
+          
+        </form>
+      </div>
+    )
+  }
+};
 export default ToDoList;
