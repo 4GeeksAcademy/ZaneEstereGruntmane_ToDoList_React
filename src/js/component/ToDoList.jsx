2623c126b6ada2b999c7054f1820d7120bf103e9
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-22 21:22:07 +0000

    todo list for submitting

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 
-const Home = () => {
-	const [todoList, setTodoList] = useState([{ label: "Sample Todo", done: false }])
+const TodoList = () => {
+	const [todoList, setTodoList] = useState([])
 	const [newTask, setNewTask] = useState("")
 	const handleDelete = (index) => {
 		let aux = todoList
@@ -23,36 +23,48 @@ const Home = () => {
 	return (
 		<div className="todo-list container-fluid mt-5 mb-5 text-center">
 
-			<form className="todo-form" onSubmit={(e) => handleSubmit(e)}>
+			<form className="todo-form mt-5" onSubmit={(e) => handleSubmit(e)}>
 				<h1 className="list-title"> Your todo list <i className="fa-solid fa-arrows-turn-to-dots" style={{ color: "#e1e8f5" }}></i> </h1>
-				<input className="text-input" type="text" value={newTask} onChange={(e) => handleChange(e)} placeholder="What do you want to do?..." />
-				<input className="button btn btn-secondary rounded-1 p-1" type="submit" value={"Add task"} />
+				<input className="text-input rounded" type="text" value={newTask} onChange={(e) => handleChange(e)} placeholder="What do you want to do?..." />
+				<input className="button btn btn-secondary btn-sm rounded p-0.1 m-2" type="submit" value={"Add task"} />
 			</form>
 
 			{todoList.length > 0 ? (
-				<div class="container-fluid">
+				<div className="container-fluid">
 					<div className="todo-items justify-content-around align-items-center bg-light shadow-sm bg-body-tertiary rounded m-auto” style={{ width: “30rem” }}">
 
 						{todoList.map((task, index) => (
 							<div className="icon-container" key={index}>
-								<div>
-									<i className="fa-regular fa-square-check" style={{ color: "#23C474", marginLeft: "5rem" }}></i>
-									{task.label}
-									<span onClick={(e) => handleDelete(index)}>
-										<i className="fa-solid fa-delete-left" style={{ color: "#FF0000", marginRight: "5rem" }}></i>
-									</span>
 
+								<i className="task-check fa-regular fa-square-check" style={{ color: "#23C474" }}></i>
+								<div className="task-label">
+									{task.label}
 								</div>
-							</div>
-						))}
+								<span onClick={(e) => handleDelete(index)}>
+									<i className="task-erase fa-solid fa-delete-left" style={{ color: "#FF0000" }}></i>
+								</span>
+
+							</div>))
+						}
 
 					</div>
+
+					<p className="counter"> {
+						todoList.length === 0 ? null :
+							todoList.length === 1 ? `${todoList.length} item` :
+								`${todoList.length} items`
+					}
+					</p>
+
+
 				</div>
 			) : (
-				<p > try adding some tasks you wish to do!</p>
-			)}
-		</div>
+				<p className="intro">  add some tasks you wish to do
+					<i className="fa-regular fa-pen-to-square fa-beat fa-xs ms-2"></i> </p>
+
+			)
+			}
+		</div >
 	);
 };
-
-export default Home;
+export default TodoList;
