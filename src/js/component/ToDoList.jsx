d7bb896418867705e5e61a105c564ae96568f78a
Zane Estere Gruntmane <141255754+esterzane@users.noreply.github.com>
2023-11-22 13:26:32 +0000

    reworked code without styling

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,56 +1,47 @@
 import React, { useState } from "react";
-import Input from "../component/Input.jsx";
 
-const ToDoList = () => {
-  const [inputValue, setInputValue] = useState("")
-  const [todos, setTodos] = useState([]);
 
-  const handleKeyPress = (e) => {
+const Home = () => {
+	const [todoList, setTodoList] = useState([{ label: "Sample Todo", done: false }])
+	const [newTask, setNewTask] = useState("")
+	const handleDelete = (index) => {
+		let aux = todoList
+		aux.splice(index, 1)
+		setTodoList([...aux])
+	}
+	const handleSubmit = e => {
+		e.preventDefault();
+		setTodoList([...todoList, { label: newTask, done: false }]);
+		setNewTask("")
+	}
+	const handleChange = e => {
+		setNewTask(e.target.value)
+		if (e.key == "Enter") {
+			handleSubmit()
+		}
+	}
+	return (
+		<div className="List-Your-Todo container-fluid mt-5 mb-5 text-center">
+		  <form onSubmit={(e) => handleSubmit(e)}>
+			<input type="text" value={newTask} onChange={(e) => handleChange(e)} placeholder="What do you want to do?..." />
+			<input type="submit" className="btn btn-secondary rounded-1 p-1" value={"Add task"} />
+		  </form>
 
-    if (e.key === "Enter") {
-      e.preventDefault()
-      setTodos([...todos, e.target.value])
-      setInputValue("")
-    }
-  };
-
-  return (
-
-      <div className="List-Your-Todo container-fluid mt-5 mb-5">
-        <form className="list-input text-center">
-          <h1 className="list-title">To Do List</h1>
-
-          <div className="container-fluid">
-            <input
-              className="input-control border-dark-subtle"
-              style={{ width: "30rem" }}
-              type="text"
-              placeholder="What do you want to do?"
-              aria-label="todo list input field"
-              onChange={(e) => setInputValue(e.target.value)}
-              value={inputValue}
-              onKeyDown={handleKeyPress}
-
-            />
-          </div>
-          {todos.map((todo, index) => (
-            <Input
-              key = {index}
-              todo = {todo}
-              todos-trash={() => setTodos(todos.filter((_, currentIndex) => index !== currentIndex))}
-            />
-          ))}
-
-        </form>
-
-        <p className="counter">
-          {
-            todos.length === 0 ? null :
-              todos.length === 1 ? `${todos.length} item` :
-                `${todos.length} items`
-          }
-        </p>
-      </div>
-  )
-};
-export default ToDoList;
+		  {todoList.length > 0 ? (
+			<div className="container-fluid todo-items bg-light shadow-sm bg-body-tertiary rounded m-auto">
+			  <i className="fa-regular fa-square-check ms-3" style={{ color: "#23C474" }}>
+				{todoList.map((task, index) => (
+				  <i key={index}>
+					{task.label} <span onClick={(e) => handleDelete(index)}>X</span>
+				  </i>
+				))}
+			  </i>
+			</div>
+		  ) : (
+			<p>try adding some tasks to do!</p>
+		  )}
+		</div>
+	  );
+	};
+	
+	export default Home;
